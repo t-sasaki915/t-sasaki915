69033873d1dee4c86ed68384883417c8fde20698
Toma Sasaki <netst915@gmail.com>
2025-06-09 12:41:21 +0900

    update home

--- app/Page/Home.hs
+++ app/Page/Home.hs
@@ -12,5 +12,31 @@ instance Page HomePage where
     pageContent HomePage =
         defaultPageLayout $
             [shamlet|
-                <p>test
+                <h1>About Me
+                <p>Haskell enjoyer, amateur violin player, Mozart lover and university student.
+
+                <h2>Basic informations
+                <span>Name: Toma Sasaki<br>
+                <span>Birthday: 15 September 2006<br>
+                <span>Major: Maths and Computer Science<br>
+                <span>Residence: England and Japan (when I'm on a long holiday)<br>
+
+                <h2>Skills
+                <ul>
+                    <li>Programming with Haskell
+                    <li>Programming with Scala
+
+                <h2>Languages I speak
+                <ul>
+                    <li>Japanese (ja_JP, very confident)
+                    <li>English (en_GB, confident)
+
+                <h2>Affiliations
+                <ul>
+                    <li>The University of Manchester (undergraduate student)
+
+                <footer>
+                    <a href="https://github.com/t-sasaki915/t-sasaki915">This page is written in Haskell
+                    \
+                    because why not.
             |]
